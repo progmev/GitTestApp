9fda5cf676e135035f56551fbc8d6e08653df231
Martynov Evgeny <martynov@wimix.by>
2022-10-25 20:15:36 +0300

    Project created

---
+++ GitTestApp/GitTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTestApp
+//
+//  Created by Martynov Evgeny on 25.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
